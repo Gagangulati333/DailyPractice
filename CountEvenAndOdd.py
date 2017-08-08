1497e751efac0e65058f62e1d27b251538a3d365
Gagan Gulati <gagangulati333@gmail.com>
2017-08-08 10:42:33 +0530

    program will count no. of evens and odds

---
+++ CountEvenAndOdd.py
@@ -0,0 +1,9 @@
+count_odd=0
+count_even=0
+for x in range(1,100):
+    if  not x%2:
+        count_even+=1
+    else:
+        count_odd+=1
+print ("no. of even numbers :",count_even)
+print ("no. of odd numbers :",count_odd)
